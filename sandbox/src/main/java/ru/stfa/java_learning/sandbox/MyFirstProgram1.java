f0c1ee9e0d6c07046d31498408d97a6ecb0bbb3a
SLukyanenko <slukyanenko@solit-clouds.ru>
2020-12-06 19:44:56 +0300

    Создан пакет для моей первой программы

--- sandbox/src/main/java/ru/stfa/java_learning/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stfa/java_learning/sandbox/MyFirstProgram1.java
@@ -1,4 +1,6 @@
-public class MyFirstProgram {
+package ru.stfa.java_learning.sandbox;
+
+public class  MyFirstProgram1 {
 	public static void main(String[] args) {
 		System.out.println("Hello!");
 	}
