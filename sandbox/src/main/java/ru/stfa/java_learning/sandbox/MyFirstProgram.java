ab3e1f9ba296545236f5c817ec6b6f3995198b96
SLukyanenko <slukyanenko@solit-clouds.ru>
2020-12-06 19:56:02 +0300

    Настройки Айдиа

---
+++ sandbox/src/main/java/ru/stfa/java_learning/sandbox/MyFirstProgram.java
@@ -0,0 +1,8 @@
+package ru.stfa.java_learning.sandbox;
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+
+}
--- sandbox/src/main/java/ru/stfa/java_learning/sandbox/MyFirstProgram1.java
+++
@@ -1,8 +0,0 @@
-package ru.stfa.java_learning.sandbox;
-
-public class  MyFirstProgram1 {
-	public static void main(String[] args) {
-		System.out.println("Hello!");
-	}
-
-}
